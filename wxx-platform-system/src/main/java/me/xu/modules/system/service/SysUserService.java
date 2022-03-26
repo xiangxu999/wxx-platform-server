9748aa3a69f6a6ef675b5cd9ab138efc98c60aa3
文享旭 <846212939@qq.com>
2022-03-26 12:30:46 +0800

    refactor：系统用户接口重构

--- wxx-platform-system/src/main/java/me/xu/modules/system/service/SysUserService.java
+++ wxx-platform-system/src/main/java/me/xu/modules/system/service/SysUserService.java
@@ -1,13 +1,15 @@
 package me.xu.modules.system.service;
 
-import me.xu.modules.system.pojo.SysUser;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import me.xu.modules.system.pojo.SysUser;
+import me.xu.modules.system.service.dto.UserQueryParam;
 
 import java.util.List;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
  * @author 旭日
@@ -16,28 +18,79 @@ import java.util.List;
 public interface SysUserService extends IService<SysUser> {
 
     /**
-     * 根据用户id返回用户对象
-     * @param userId 用户id
-     * @return 用户对象
+     * 根据分页和查询参数进行查询
+     *
+     * @param page       分页
+     * @param queryParam 查询参数
+     * @return 用户列表
      */
-    SysUser getByUserId(Long userId);
+    Page<SysUser> userList(Page<SysUser> page, UserQueryParam queryParam);
+
 
     /**
-     * 根据用户名返回用户对象
-     * @param username 用户名
-     * @return 用户对象
+     * 根据id获取用户的信息
+     *
+     * @param id 用户id
+     * @return 用户
      */
-    SysUser getByUsername(String username);
+    SysUser userInfo(Long id);
+
+    /**
+     * 用户新增
+     *
+     * @param sysUser 用户对象
+     * @return Boolean
+     */
+    Boolean userSave(SysUser sysUser);
+
+
+    /**
+     * 用户修改
+     *
+     * @param sysUser 用户对象
+     * @return Boolean
+     */
+    Boolean userUpdate(SysUser sysUser);
+
+
+    /**
+     * 用户删除
+     *
+     * @param ids 用户对象ids
+     * @return Boolean
+     */
+    Boolean userDelete(Long[] ids);
+
+
+    /**
+     * 用户角色分配
+     *
+     * @param id      用户id
+     * @param roleIds 角色ids
+     * @return Boolean
+     */
+    Boolean roleChange(Long id, Long[] roleIds);
+
+    /**
+     * 用户重置密码
+     *
+     * @param id 用户id
+     * @return Boolean
+     */
+    Boolean repass(Long id);
+
 
     /**
      * 返回一个账号所拥有的角色标识集合
+     *
      * @param userId 用户id
-     * @return
+     * @return 角色id集合
      */
     List<Long> getUserRoleList(Long userId);
 
     /**
      * 根据角色id返回对应的用户列表
+     *
      * @param roleId 角色id
      * @return 用户列表
      */
@@ -45,8 +98,11 @@ public interface SysUserService extends IService<SysUser> {
 
     /**
      * 根据菜单id返回对应的用户列表
+     *
      * @param menuId 菜单id
      * @return 用户列表
      */
     List<SysUser> getUserListByMenuId(Long menuId);
+
+
 }
--- wxx-platform-system/src/main/java/me/xu/modules/system/service/impl/SysUserServiceImpl.java
+++ wxx-platform-system/src/main/java/me/xu/modules/system/service/impl/SysUserServiceImpl.java
@@ -1,21 +1,39 @@
 package me.xu.modules.system.service.impl;
 
+import cn.dev33.satoken.secure.SaSecureUtil;
+import cn.dev33.satoken.session.SaSession;
+import cn.dev33.satoken.stp.StpUtil;
+import cn.hutool.core.date.DateUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import me.xu.common.ResultCode;
+import me.xu.consts.CommonConst;
+import me.xu.exception.ResultException;
 import me.xu.modules.system.mapper.SysUserMapper;
 import me.xu.modules.system.pojo.SysRole;
 import me.xu.modules.system.pojo.SysUser;
+import me.xu.modules.system.pojo.SysUserRole;
 import me.xu.modules.system.service.SysRoleService;
+import me.xu.modules.system.service.SysUserRoleService;
 import me.xu.modules.system.service.SysUserService;
+import me.xu.modules.system.service.dto.UserQueryParam;
+import me.xu.utils.QueryHelpUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 旭日
@@ -30,15 +48,120 @@ public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> impl
     @Autowired
     private SysRoleService sysRoleService;
 
+    @Autowired
+    private SysUserRoleService sysUserRoleService;
 
     @Override
-    public SysUser getByUserId(Long userId) {
-        return getOne(new QueryWrapper<SysUser>().eq("user_id", userId));
+    public Page<SysUser> userList(Page<SysUser> page, UserQueryParam queryParam) {
+        // 查询出来结果
+        Page<SysUser> result = page(page, QueryHelpUtil.getPredicate(queryParam));
+        // 对结果每一项进行处理，添加角色和把密码置为空
+        result.getRecords().forEach(sysUser -> {
+            // 密码置为空
+            sysUser.setPassword("");
+            // 查询该用户对应的角色
+            List<SysUserRole> sysUserRoles = sysUserRoleService.list(new QueryWrapper<SysUserRole>().eq("user_id", sysUser.getUserId()));
+            if (sysUserRoles.size() > 0) {
+                List<Long> roleIds = sysUserRoles.stream().map(SysUserRole::getRoleId).collect(Collectors.toList());
+                List<SysRole> roles = sysRoleService.listByIds(roleIds);
+                sysUser.setRoles(roles);
+            }
+        });
+        return result;
     }
 
     @Override
-    public SysUser getByUsername(String username) {
-        return getOne(new QueryWrapper<SysUser>().eq("username",username));
+    public SysUser userInfo(Long id) {
+        SysUser result = getById(id);
+        result.setPassword("");
+        List<SysRole> sysRoles = sysRoleService.getSysRoleListByUserId(id);
+        if (sysRoles.size() > 0) {
+            result.setRoles(sysRoles);
+        }
+        return result;
+    }
+
+    @Override
+    public Boolean userSave(SysUser sysUser) {
+        // 设置默认密码
+        sysUser.setPassword(SaSecureUtil.md5(CommonConst.USER_PASSWORD));
+        // 设置默认头像
+        sysUser.setAvatar("https://cdn.lixingyong.com/2021/01/15/QQ20210115152209.jpg");
+        return save(sysUser);
+    }
+
+    @Override
+    public Boolean userUpdate(SysUser sysUser) {
+        return updateById(sysUser);
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    public Boolean userDelete(Long[] ids) {
+        List<SysUser> sysUsers = listByIds(Arrays.asList(ids));
+        sysUsers.forEach(sysUser -> {
+            // 清除缓存
+            SaSession session = StpUtil.getSessionByLoginId(sysUser.getUserId(), false);
+            if (session != null) {
+                session.delete("Role_List");
+            }
+            // 删除中间表
+            boolean result = sysUserRoleService.remove(new QueryWrapper<SysUserRole>().eq("user_id", sysUser.getUserId()));
+            if (!result) {
+                throw new ResultException(ResultCode.FAILED);
+            }
+        });
+        return removeByIds(Arrays.asList(ids));
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    public Boolean roleChange(Long id, Long[] roleIds) {
+        // 分配角色为空
+        if (roleIds.length == 0) {
+            throw new ResultException(ResultCode.ROLE_EMPTY);
+        }
+        List<SysUserRole> sysUserRoles = new ArrayList<>();
+
+        // 构造中间表数据
+        Arrays.stream(roleIds).forEach(roleId -> {
+            SysUserRole sysUserRole = new SysUserRole();
+            sysUserRole.setUserId(id);
+            sysUserRole.setRoleId(roleId);
+            sysUserRoles.add(sysUserRole);
+        });
+
+        // 清除缓存
+        SysUser sysUser = getById(id);
+        SaSession session = StpUtil.getSessionByLoginId(sysUser.getUserId(), false);
+        if (session != null) {
+            session.delete("Role_List");
+        }
+
+        // 删除之前的记录
+        sysUserRoleService.remove(new QueryWrapper<SysUserRole>().eq("user_id", id));
+        // 添加记录
+        if (sysUserRoleService.saveBatch(sysUserRoles)) {
+            return true;
+        } else {
+            throw new ResultException(ResultCode.FAILED);
+        }
+    }
+
+    @Override
+    public Boolean repass(Long id) {
+        SysUser sysUser = getById(id);
+        String password = SaSecureUtil.md5(CommonConst.USER_PASSWORD);
+        sysUser.setPassword(password);
+        // 记录重置密码时间
+        sysUser.setPwdResetTime(LocalDateTime.parse(DateUtil.now(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        boolean result = updateById(sysUser);
+        if (result) {
+            // 重置密码后，直接下线
+            StpUtil.logout(id);
+            return true;
+        }
+        return false;
     }
 
     @Override
@@ -61,5 +184,6 @@ public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> impl
     public List<SysUser> getUserListByMenuId(Long menuId) {
         return sysUserMapper.getSysUserListByMenuId(menuId);
     }
+
 
 }
